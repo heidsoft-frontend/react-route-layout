5093f1aa1ae1821b046b7d5bb511b191ad6d82db
jake.liu <jake.liu@baozun.com>
2023-01-02 21:32:36 +0800

    布局，选择当前菜单

--- src/App.js
+++ src/App.js
@@ -1,35 +1,93 @@
 import { Menu } from "antd"
 //图标
-import { HomeOutlined,DashboardOutlined,UnorderedListOutlined,UserOutlined,PoweroffOutlined } from "@ant-design/icons"
-import {Routes,Route} from "react-router-dom"
+import { HomeOutlined, DashboardOutlined, UnorderedListOutlined, UserOutlined, PoweroffOutlined } from "@ant-design/icons"
+import { Routes, Route, useNavigate } from "react-router-dom"
 function App() {
   return (
-    <div style={{display:'flex',flexDirection:'row'}}>
-      <Menu 
-      onClick={({key})=>{
-        console.log(key)
-      }} 
-      items={[
-        { label: 'Home',icon: <HomeOutlined/> },
-        { label: "Dashboard",icon:<DashboardOutlined/> },
-        { label: "User List",icon:<UnorderedListOutlined/> },
-        { label: "Profiles", icon:<UserOutlined/> },
-        { label: "Signout", icon: <PoweroffOutlined/>, danger:true }
-      ]}>
-      </Menu>
-      <Content/>
+    <div style={{
+      display:"flex",
+      flexDirection: "column",
+      flex:1,
+      height:"100vh"
+    }}>
+      <Headers />
+      <div style={{
+        display:"flex",
+        flexDirection: "row",
+        flex:1
+      }}>
+        <SideMenu />
+        <Content />
+      </div>
+      <Fouter />
     </div>
   );
 }
 
-function Content(){
+function Headers() {
+  return (<div 
+    style={{ 
+      height: 60, 
+      backgroundColor: "lightblue", 
+      color: "black", 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      fontWeight: 'bold'
+    }}>Headers</div>);
+}
+
+function Fouter() {
+  return (<div 
+    style={{ 
+      height: 60, 
+      backgroundColor: "lightgray", 
+      color: "white", 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      fontWeight: 'bold'
+    }}>Fouter</div>);
+}
+
+function SideMenu() {
+  const navigate = useNavigate()
+  return (<div style={{ display: 'flex', flexDirection: 'row' }}>
+    <Menu
+      onClick={({ key }) => {
+        if (key === 'signout') {
+          // 登出操作
+        } else {
+          console.log(key)
+          navigate(key);
+        }
+      }}
+      defaultSelectedKeys={[window.location.pathname]}
+      items={[
+        { label: 'Home', key: '/', icon: <HomeOutlined /> },
+        { label: "Dashboard", key: '/dashboard', icon: <DashboardOutlined /> },
+        { label: "User List", key: '/usersList', icon: <UnorderedListOutlined />,
+        children:[
+          {label: "活动用户", key: '/activeUsers'},
+          {label: "禁用用户", key: '/disableUsers'}
+        ]},
+        { label: "Profiles", key: '/profile', icon: <UserOutlined /> },
+        { label: "Signout", key: 'signout', icon: <PoweroffOutlined />, danger: true }
+      ]}>
+    </Menu>
+  </div>);
+}
+
+function Content() {
   return <div>
-   <Routes>
-    <Route path="/" element={<div>Home</div>}></Route>
-    <Route path="/dashboard" element={<div>Dashboard</div>}></Route>
-    <Route path="/profile" element={<div>Profiles</div>}></Route>
-    <Route path="signout" element={<div>signout</div>}></Route>
-   </Routes>
+    <Routes>
+      <Route path="/" element={<div>Home</div>}></Route>
+      <Route path="/dashboard" element={<div>Dashboard</div>}></Route>
+      <Route path="/activeUsers" element={<div>活动 List</div>}></Route>
+      <Route path="/disableUsers" element={<div>禁用 List</div>}></Route>
+      <Route path="/profile" element={<div>Profiles</div>}></Route>
+      <Route path="signout" element={<div>signout</div>}></Route>
+    </Routes>
   </div>;
 }
 
