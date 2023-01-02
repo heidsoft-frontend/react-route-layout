8d6891d8bb90b3484e9c1291feae72634fe3dad7
jake.liu <jake.liu@baozun.com>
2023-01-02 20:19:51 +0800

    新增ant-design icon图标与路由

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { Menu } from "antd"
+//图标
+import { HomeOutlined,DashboardOutlined,UnorderedListOutlined,UserOutlined,PoweroffOutlined } from "@ant-design/icons"
+import {Routes,Route} from "react-router-dom"
 function App() {
   return (
-    <div>
+    <div style={{display:'flex',flexDirection:'row'}}>
       <Menu items={[
-        { label: 'Home' },
-        { label: "Dashboard" },
-        { label: "User" },
-        { label: "Profiles" },
-        { label: "Signout" }
+        { label: 'Home',icon: <HomeOutlined/> },
+        { label: "Dashboard",icon:<DashboardOutlined/> },
+        { label: "User List",icon:<UnorderedListOutlined/> },
+        { label: "Profiles", icon:<UserOutlined/> },
+        { label: "Signout", icon: <PoweroffOutlined/>, danger:true }
       ]}>
       </Menu>
+      <Content/>
     </div>
   );
+}
+
+function Content(){
+  return <div>
+   <Routes>
+    <Route path="/" element={<div>Home</div>}></Route>
+    <Route path="/dashboard" element={<div>Dashboard</div>}></Route>
+    <Route path="/profile" element={<div>Profiles</div>}></Route>
+    <Route path="signout" element={<div>signout</div>}></Route>
+   </Routes>
+  </div>;
 }
 
 export default App;
